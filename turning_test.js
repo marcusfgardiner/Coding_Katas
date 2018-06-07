6c04841dca05c6a2f6407c95f5c0d37b45a14d29
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-07 10:49:46 +0100

    Solved second kata

--- turning_test.js
+++ turning_test.js
@@ -1,7 +1,16 @@
+const add = (a, b) => Number(a) + Number(b);
+
 function isTuringEquation(s) {
+  let reversedString = s
+    .split("")
+    .reverse()
+    .join("");
 
-s.split("")
-  .reverse()
-  .join("");
-
+  let splitString = reversedString.split("=");
+  let answer = Number(splitString[0]);
+  let numbersToSum = splitString[1].split("+");
+  let summedNumbers = numbersToSum.reduce(add);
+  console.log(answer);
+  console.log(summedNumbers);
+  return answer === summedNumbers;
 }
