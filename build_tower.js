3637661ef03ea7b3f9d3e98a07e4d9ceaf34ff76
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-08 11:55:04 +0100

    Kata 4 complete

--- build_tower.js
+++ build_tower.js
@@ -1,7 +1,20 @@
-function towerBuilder(nFloors) {
-  let starCount = 1 + ((nFloors - 1)*2)
-  return "*".repeat(starCount);
+var starForSpace = (string, number) => {
+    newString = string.slice(number, -number);
+    return (" ".repeat(number) + newString + " ".repeat(number));
 }
+
+function towerBuilder(nFloors) {
+  let tower = []
+  let starCount = 1 + ((nFloors - 1)*2);
+  bottomRow = "*".repeat(starCount);
+  tower.push(bottomRow);
+  for(var i = 1; i < nFloors; i++) {
+  currentRow = tower[0]
+  tower.unshift(starForSpace(currentRow, i));
+  }
+  return tower;
+}
+
 
 console.log(towerBuilder(1))
 console.log(towerBuilder(2))
